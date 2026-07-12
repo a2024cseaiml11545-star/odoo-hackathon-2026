6ba96b348d679ce72e7eff7cfbd92b3de0f05e5a
Riya Agarwal <a2024cseaiml11545@imsec.ac.in>
2026-07-12 13:50:56 +0530

    Added RBAC and Dashboard

---
+++ client-new/src/data/user.js
@@ -0,0 +1,19 @@
+const users = [
+  {
+    email: "admin@gmail.com",
+    password: "1234",
+    role: "Administrator",
+  },
+  {
+    email: "manager@gmail.com",
+    password: "1234",
+    role: "Manager",
+  },
+  {
+    email: "driver@gmail.com",
+    password: "1234",
+    role: "Driver",
+  },
+];
+
+export default users;
